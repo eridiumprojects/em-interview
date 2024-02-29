f6e4b4968df7756954080703ecfc6ceff9a0ee47
eridium <pestrikv1337@gmail.com>
2024-03-01 00:17:01 +0300

    Check pipeline processing

--- src/test/java/com/example/e_m_test/EMTestApplicationTests.java
+++ src/test/java/com/example/e_m_test/EMTestApplicationTests.java
@@ -1,15 +1,12 @@
 package com.example.e_m_test;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class EMTestApplicationTests {
-
     @Test
-    @Disabled
-    void contextLoads() {
+    void test() {
     }
 
 }
